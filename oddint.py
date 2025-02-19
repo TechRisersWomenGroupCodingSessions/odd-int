eadf046b7caded8a1420a4473452d6c89426baaa
Preethi Evelyn Sadanandan <57084343+evil-in@users.noreply.github.com>
2025-02-19 18:50:30 +0000

    Worked on function for test 3: WIP

--- oddint.py
+++ oddint.py
@@ -1,5 +1,13 @@
-def isOdd(num_list):
-    dist_vals=list(set(num_list))
-    for i in num_list:
+def isOdd(num_list): # [1,2,2]
+    dist_vals=list(set(num_list)) # [1,2]
+    dict_vals_count = {key: -1 for key in dist_vals}
+    print(f"Default value of dict: {dict_vals_count}")
+    for i in dist_vals:
+        print(f"Loop of i: {i}")
+        for j in num_list:
+            print(f"Loop of j: {j}")
+
+
+
         
     return dist_vals
