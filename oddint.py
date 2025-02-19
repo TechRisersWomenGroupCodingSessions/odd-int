db8975e8c8ac4939b62cbcef20fa6e6f548d7684
Emma Gale <emmajgale@hotmail.com>
2025-02-19 19:05:27 +0000

    third test passes

--- oddint.py
+++ oddint.py
@@ -1,13 +1,17 @@
 def isOdd(num_list): # [1,2,2]
     dist_vals=list(set(num_list)) # [1,2]
-    dict_vals_count = {key: -1 for key in dist_vals}
+    dict_vals_count = {key: 0 for key in dist_vals}
     print(f"Default value of dict: {dict_vals_count}")
     for i in dist_vals:
-        print(f"Loop of i: {i}")
+        # print(f"Loop of i: {i}")
         for j in num_list:
-            print(f"Loop of j: {j}")
+            # print(f"Loop of j: {j}")
+            if i == j:
+                dict_vals_count[i] += 1;
+    print(f"value of dict after loop {dict_vals_count}")
+  
+    for key, value in dict_vals_count.items():
+        if value == 1:
+          return key
 
-
-
-        
-    return dist_vals
+    
