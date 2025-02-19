4dbadbe7180efbe56699534825373fe75165973d
Emma Gale <emmajgale@hotmail.com>
2025-02-19 18:20:20 +0000

    test three integers fails

--- test_oddint.py
+++ test_oddint.py
@@ -4,6 +4,10 @@ def test_one_integer():
     num_list = [7]
     assert isOdd(num_list) == 7
 
-def test_two_integer():
+def test_zero():
     num_list = [0]
     assert isOdd(num_list) == 0
+
+def test_three_integers():
+    num_list = [1,1,2]
+    assert isOdd(num_list) == 2
