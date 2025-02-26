138141ea2c4ab42e0a18f7f08f1c3e9e85fe9952
Emma Gale <emjogale@users.noreply.github.com>
2025-02-26 18:40:25 +0000

    add test for non-valid input

--- test_oddint.py
+++ test_oddint.py
@@ -13,9 +13,14 @@ def test_three_integers():
     assert isOdd(num_list) == 2
 
 def test_if_multiple_integers_returns_0():
-    num_list = [0,1,0,1,0]
+    num_list = [0,-1,0,-1,0]
     assert isOdd(num_list) == 0
 
 def test_if_multiple_integers_returns_4():
     num_list = [1,2,2,3,3,3,4,3,3,3,2,2,1]
     assert isOdd(num_list) == 4
+
+def test_non_valid_input():
+    num_list = ['forty', 'two']
+    assert isOdd(num_list) == 'Not valid input'
+
