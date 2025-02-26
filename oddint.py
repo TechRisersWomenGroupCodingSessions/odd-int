5b491268759417f4ea9e990b1c3f4f4ea4abc575
Emma Gale <emjogale@users.noreply.github.com>
2025-02-26 19:14:10 +0000

    non valid input test passes

--- oddint.py
+++ oddint.py
@@ -1,10 +1,7 @@
 def isOdd(num_list): # [1,2,2]
     for i in num_list:
+        if not isinstance(i, int):
+            return 'Not valid input'
+        
         if num_list.count(i) % 2 == 1:
             return i
-
-
-
-
-
-
